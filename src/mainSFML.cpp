0c3269d141f896e893292897bf127f93c9409c72
Antonin Slv <antonin.sylvestre@orange.fr>
2023-04-29 16:20:00 +0200

    début nombre tour et temps

--- src/mainSFML.cpp
+++ src/mainSFML.cpp
@@ -38,6 +38,7 @@ int main(){
     while (!quitter){
         string action = boucleMenu(window, volume, decalage);
         if (action == "niveau_1"){
+            
             G1.restart();
             temps_au_tour.restart();
             G1.boucleJeuSFML(window, temps_au_tour, decalage);
