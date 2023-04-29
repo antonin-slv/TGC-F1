c23fb1fe39539614c68354d4566e8619e274b7ba
Sipior <thomas.vittore@etu.univ-lyon1.fr>
2023-04-29 22:31:04 +0200

    barhain <3

--- src/mainSFML.cpp
+++ src/mainSFML.cpp
@@ -39,8 +39,7 @@ int main(){
     while (!quitter){
         string action = boucleMenu(window, volume, decalage);
         if (action == "niveau_1"){
-            
-            G1.chargerNiveau("data/circuits/niveau_1.json",2);
+            G1.chargerNiveau("data/circuits/barhain.json",2);
             temps_au_tour.restart();
             temps=G1.boucleJeuSFML(window, temps_au_tour, decalage);
         }
